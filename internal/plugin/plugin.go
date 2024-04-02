bbca0a00e70f96a2c35b89c5c721ee07e34bad0f
* <61941069+Git-Fal7@users.noreply.github.com>
2024-04-02 23:29:00 +0300

    (main) move the db variable to the database package, so that import cycles doesn't happen

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -5,28 +5,28 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/git-fal7/friend-for-gate/internal/command"
 	"github.com/git-fal7/friend-for-gate/internal/database"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"go.minekube.com/gate/pkg/edition/java/proxy"
 )
-
-var DB *database.Queries
 
 func InitPlugin(ctx context.Context, proxy *proxy.Proxy) error {
 	db, err := sql.Open("pgx", fmt.Sprintf("user=%s password=%s host=%s port=%s database=%s sslmode=disable", "admin", "adminpassword", "localhost", "5432", "friends"))
 	if err != nil {
 		return err
 	}
-	DB = database.New(db)
+	database.DB = database.New(db)
 	// Init tables
-	err = DB.CreateFriendstatusType(context.Background())
+	err = database.DB.CreateFriendstatusType(context.Background())
 	if err != nil {
 		return err
 	}
-	err = DB.CreateUsersTable(context.Background())
+	err = database.DB.CreateUsersTable(context.Background())
 	if err != nil {
 		return err
 	}
+	command.Init(proxy)
 
 	return nil
 }
